00615856c496514e75802963585a06471687dfde
NaeemHk011 <itxkhaskheli@gmail.com>
2024-04-11 17:35:26 +0500

    error solved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Nav";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import nav from "@/components/Nav";
+import Nav from "@/components/Nav";
 
 
 export default function App({ Component, pageProps }: AppProps) {
