19a8aac7d284918c2c17e201029a7dd5f0d4603e
NaeemHk011 <itxkhaskheli@gmail.com>
2024-04-11 18:17:59 +0500

    routes setting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,29 +7,29 @@ interface Props {
 
 const Navbar = ({ openNav }: Props) => {
 
-    
+
     return (<div className="w-[100%] flexed z-[10000] h-[12vh] bg-[#141c27] shadow-md" style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 10000 }}>
 
 
         <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
             <h1 className="flex-[0-6] cursor-pointer text-[25px] text-white font-bold pr-80">WEB
                 <span className="text-yellow-300">DEV</span></h1>
-            <Link href="/nav.tsx">
+            <Link href="#">
                 <span className="nav-link">HOME</span>
             </Link>
-            <Link href="/Services">
+            <Link href="#">
                 <span className="nav-link">SERVICES</span>
             </Link>
-            <Link href="/Projects.tsx">
+            <Link href="#">
                 <span className="nav-link">PROJECTS</span>
             </Link>
-            <Link href="/blog">
+            <Link href="#">
                 <span className="nav-link">BLOG</span>
             </Link>
-            <Link href="/contact">
+            <Link href="#">
                 <span className="nav-link">CONTACT</span>
             </Link>
-            <Link href="/about">
+            <Link href="#">
                 <span className="nav-link">ABOUT</span>
             </Link>
 
